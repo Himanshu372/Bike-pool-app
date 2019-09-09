9a11eb357dc984db7f05f34a44f8cf8473f440f7
Himanshu372 <jagtaphimanshu09@gmail.com>
2019-09-09 20:03:34 +0530

    Offer_ride API

---
+++ bike_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+# router.register(r'', views.home)
+router.register(r'offer_ride', views.fetchrideData)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', views.home),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
--- bike_app/views.py
+++ bike_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from bike_app.models import rideData
+from bike_app.serializers import rideDataSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -6,4 +10,30 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'bike_app/index.html')
+
+
+class fetchrideData(viewsets.ModelViewSet):
+    queryset = rideData.objects.all()
+
+
+    def create(self, request, *args, **kwargs):
+        post_data = request.data
+        serializer = rideDataSerializer(data = post_data)
+        user_id = post_data['user_id']
+        depart_time = post_data['depart_time']
+        if self.queryset.filter(user_id = user_id, depart_time = depart_time).exists():
+            return Response('You have already created a ride for given departure time')
+        else:
+            if serializer.is_valid():
+                serializer.save()
+                return Response('Your ride has been saved, expect bookings anytime')
+            else:
+                return Response('Please check your data')
+
+
+
+
+
+
+
 
