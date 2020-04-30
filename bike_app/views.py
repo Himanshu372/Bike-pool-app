357c2b39bbc5c97cdc2a573b4e10d20546c6cc8c
Himanshu372 <jagtaphimanshu09@gmail.com>
2020-04-30 17:33:17 +0530

    Changes to views.py

--- bike_app/views.py
+++ bike_app/views.py
@@ -73,8 +73,6 @@ class OfferRide(viewsets.ModelViewSet):
         # Should add a validation check here
         depart_lat_long = get_location(depart)
         arrival_lat_long = get_location(arrival)
-
-
         if is_return:
             depart_date_time = data['datetimepicker-departure']
             arrival_date_time = data['datetimepicker-arrival']
@@ -87,15 +85,19 @@ class OfferRide(viewsets.ModelViewSet):
             depart_date_time_field = datetime.strftime(datetime.strptime(depart_date_time, '%m/%d/%Y %H:%M %p'),
                                                        '%Y-%m-%d %H:%M:%S')
             arrival_date_time_field = None
-        stopover_list = []
-        for i in range(0, len(data) - 6):
-            stopover_list.append(data['stopover_' + str(i)])
-        stopover_lat_long_list = [str(get_location(k)) for k in stopover_list]
-        ride_data_obj = rideData(user_id=101, pickup=str(depart_lat_long),
-                                 dropoff=str(arrival_lat_long), stopovers=''.join(stopover_lat_long_list),
+        stopovers = getattr(data, 'stopover_0', None)
+        if stopovers:
+            stopover_list = []
+            for i in range(0, len(data) - 6):
+                stopover_list.append(data['stopover_' + str(i)])
+            stopover_lat_long_list = [str(get_location(k)) for k in stopover_list]
+            stopovers = str(stopover_lat_long_list)
+        ride_data_obj = rideData(user_id=21, pickup=str(depart_lat_long),
+                                 dropoff=str(arrival_lat_long), stopovers=stopovers,
                                  depart_time=depart_date_time_field, return_time=arrival_date_time_field,
                                  is_return=is_return)
-        if self.queryset.filter(user_id = 101, pickup = str(depart_lat_long), depart_time = depart_date_time_field).exists():
+        if self.queryset.filter(user_id=21, pickup=str(depart_lat_long),
+                                depart_time=depart_date_time_field).exists():
             return HttpResponse('A similar ride already exists')
         else:
             ride_data_obj.save()
@@ -113,34 +115,30 @@ class FindRide(viewsets.ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         form = findRideform
-        args = {'form' : form}
+        args = {'form': form}
         return render(request, self.template_name, args)
 
     def create(self, request, *args, **kwargs):
         data = request.POST
         pickup_loc = data['pickup']
         dropoff_loc = data['dropoff']
-        travel_date = data['date']
-        travel_time = data['time']
-        travel_date_time = datetime.strftime(datetime.strptime(travel_date + ' ' + travel_time, '%Y-%m-%d %H:%M'),
-                                             '%Y-%m-%d %H:%M:%S')
+        travel_date_time = data['datetimepicker-departure']
+        date_time_obj = datetime.strptime(travel_date_time, '%m/%d/%Y %H:%M %p')
 
         # Should include a validation check here
         pickup_lat_long = get_location(pickup_loc)
         dropoff_lat_long = get_location(dropoff_loc)
-        rides = self.queryset.filter(depart_time__date=travel_date)
+        rides = self.queryset.filter(depart_time__date=date_time_obj.date())
         results = []
         for each_ride in rides:
-            dist = cal_haversine_distance(str(pickup_lat_long), each_ride.pickup)
-            # results[each_ride.user_id] = {}
-            # results[each_ride.user_id]['distance'] = dist
-            # results[each_ride.user_id]['depart_time'] = datetime.strftime(each_ride.depart_time.date(), '%Y-%m-%d')
-            # results[each_ride.user_id]['fare'] = round(50 + 12 * dist)
-            respone_obj = findRideResponse(user_id=each_ride.user_id, distance=dist,
+            pickup_dist = cal_haversine_distance(str(pickup_lat_long), each_ride.pickup)
+            dropoff_dist = cal_haversine_distance(str(dropoff_lat_long), each_ride.dropoff)
+            distance_travelled = cal_haversine_distance(each_ride.pickup, each_ride.dropoff)
+            respone_obj = findRideResponse(user_id=each_ride.user_id, distance=pickup_dist,
                                            depart_time=datetime.strftime(each_ride.depart_time.date(), '%Y-%m-%d'),
-                                           fare=round(50 + 12 * dist))
+                                           fare=round(50 + 12 * distance_travelled))
             results.append(respone_obj)
-        results = sorted(results, key = lambda x : x.distance, reverse = True)
+        results = sorted(results, key=lambda x: x.distance, reverse=True)
         serializer = findRideResponseSerializer()
 
         results = serializer.serialize(results)
