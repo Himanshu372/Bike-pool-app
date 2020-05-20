80fbc4b80d8c9fe5e6643bcefb9a9733a4b53458
Himanshu372 <jagtaphimanshu09@gmail.com>
2020-05-21 00:44:37 +0530

    Added test cases for offer_ride view

--- bike_app/tests/test_views.py
+++ bike_app/tests/test_views.py
@@ -2,9 +2,9 @@ import mock
 from mock import Mock
 from django.test import TestCase, Client
 from django.urls import reverse
-from ..forms import userSignup
+from ..forms import userSignup, findRideform
 from ..models import rideData
-from ..views import OfferRide
+from ..views import OfferRide, FindRide
 
 class TestHomeView(TestCase):
     def setUp(self):
@@ -62,3 +62,31 @@ class TestOfferRideViewSet(TestCase):
         mock_filter.return_value.exists.return_value = True
         post_response_with_existing_object = self.client.post((reverse('offer_ride')), data=self.post_data)
         self.assertEqual(post_response_with_existing_object.content.decode('utf-8'), 'A similar ride already exists')
+
+    def test_offer_ride_get_success(self):
+        get_response = self.client.get(reverse('offer_ride'))
+        self.assertTemplateUsed(get_response, 'bike_app/offer_ride.html')
+
+
+class TestFindRideViewSet(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.post_data = {'pickup': 'Bremen Chowk, Vidyapeeth Road, Pune University, Aundh, Pune, Maharashtra, India',
+                          'dropoff': 'B.A.R.C, Trombay, Mumbai, Maharashtra, India',
+                          'datetime': '05/17/2020 12:00 PM'
+        }
+
+    def test_find_ride_get_success(self):
+        get_response = self.client.get(reverse('find_ride'))
+        self.assertEqual(get_response.status_code, 200)
+        self.assertTemplateUsed(get_response, 'bike_app/find_ride.html')
+
+    def test_find_ride_post_success(self):
+        post_response = self.client.post(reverse('find_ride'), data=self.post_data)
+        self.assertEqual(post_response.status_code, 200)
+
+    @mock.patch.object(FindRide.queryset, "filter")
+    def test_find_ride_post_no_ride_found(self, mock_filter):
+        mock_filter.return_value = []
+        post_response = self.client.post(reverse('find_ride'), data=self.post_data)
+        self.assertEqual(post_response.json(), 'No Rides found')
