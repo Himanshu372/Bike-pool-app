288067d897a45144bb4d49fcac48fbd098fda165
Himanshu Jagtap <jagtaphimanshu09@gmail.com>
2021-05-29 12:44:01 +0530

    More changes for circleCI to function

--- Bike_Pool_App/settings.py
+++ Bike_Pool_App/settings.py
@@ -168,6 +168,7 @@ SOCIAL_AUTH_PIPELINE = (
 
 # Password Hasher
 PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
