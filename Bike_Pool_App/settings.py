63470bfb12c610290496e8ab985efc684e9c8a2d
Himanshu Jagtap <jagtaphimanshu09@gmail.com>
2021-05-29 12:34:54 +0530

    Removing argon from hasher

--- Bike_Pool_App/settings.py
+++ Bike_Pool_App/settings.py
@@ -168,7 +168,6 @@ SOCIAL_AUTH_PIPELINE = (
 
 # Password Hasher
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
